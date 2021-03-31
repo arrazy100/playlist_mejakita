5ecb59c071d55f438a7abf1b0d783beeb6875914
afar0308 <afdhalarrazy111@gmail.com>
2021-03-31 15:13:02 +0700

    Fix responsive

--- app/Views/users/playlist.php
+++ app/Views/users/playlist.php
@@ -5,47 +5,50 @@
 <main>
     <div class="row">
 
-        <!-------------------------------------------- BANNER -------------------------------------------->
+        <!-- BANNER SECTION -->
 
         <div class="col-12 banner">
             <div class="container">
                 <div class="row">
                     <!-- IMAGE BANNER -->
-                    <div class="col-6">
-                        <img class="ban-image" src="<?= base_url() ?>/assets/img/image.png" alt="">
+                    <div class="col-12 col-md-6">
+                        <img class="ban-image img-fluid" src="<?= base_url() ?>/assets/img/3255539.jpg" alt="">
                     </div>
 
                     <!-- TEXT AND BUTTON -->
 
-                    <div class="col-6 text-btn">
+                    <div class="col-12 col-md-6 text-btn">
 
                         <!-- PLAYLIST -->
 
-                        <div class="col-md-12">
-                            <h1 class="ban-text">PlayList Belajar</h1>
+                        <div class="col-12">
+                            <h1 class="ban-text">Playlist Belajar</h1>
                         </div>
 
                         <!-- DETAIL PLAYLIST -->
 
-                        <div class="col-md-12">
+                        <div class="col-12">
                             <p class="ban-text-detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat.</p>
                         </div>
 
                         <!-- BOOKMARKED -->
 
-                        <div class="col-md-12">
-                            <button type="button" class="btn btn-primary float-right">Bookmarked</button>
+                        <div class="col-12 btn-bookmarked">
+                            <button type="button" class="btn btn-bookmark float-md-right">Bookmarked</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- BANNER SECTION END -->
+
         <div class="container">
             <div class="row">
 
-                <!-------------------------------------------- REKOMENDASI ------------------------------------------>
+                <!-- REKOMENDASI SECTION -->
 
-                <div class="col-8">
+                <div class="col-12 col-lg-8">
                     <div class="row">
                         <div class="col-12">
                             <h5>Rekomendasi</h5>
@@ -53,9 +56,9 @@
 
                         <!-- START -->
 
-                        <div class="col-4">
+                        <div class="col-12 col-sm-6 col-xl-4">
                             <div class="rekomen-img">
-                                <img src="<?= base_url() ?>/assets/img/image.png" class="img-thumbnail" alt="...">
+                                <img src="<?= base_url() ?>/assets/img/hero_img.png" class="img-thumbnail" alt="...">
                                 <div class="row">
                                     <div class="col-12">
                                         <p class="judul-rekomen"><b>Biologi - DNA</b></p>
@@ -75,7 +78,7 @@
 
                         <!-- END 1 -->
 
-                        <div class="col-4">
+                        <div class="col-12 col-sm-6 col-xl-4">
                             <div class="rekomen-img">
                                 <img src="<?= base_url() ?>/assets/img/image.png" class="img-thumbnail" alt="...">
                                 <div class="row">
@@ -97,7 +100,7 @@
 
                         <!-- END2 -->
 
-                        <div class="col-4">
+                        <div class="col-12 col-sm-6 col-xl-4">
                             <div class="rekomen-img">
                                 <img src="<?= base_url() ?>/assets/img/image.png" class="img-thumbnail" alt="...">
                                 <div class="row">
@@ -119,7 +122,7 @@
 
                         <!-- END 3 -->
 
-                        <div class="col-4">
+                        <div class="col-12 col-sm-6 col-xl-4">
                             <div class="rekomen-img">
                                 <img src="<?= base_url() ?>/assets/img/image.png" class="img-thumbnail" alt="...">
                                 <div class="row">
@@ -141,7 +144,7 @@
 
                         <!-- END 4 -->
 
-                        <div class="col-4">
+                        <div class="col-12 col-sm-6 col-xl-4">
                             <div class="rekomen-img">
                                 <img src="<?= base_url() ?>/assets/img/image.png" class="img-thumbnail" alt="...">
                                 <div class="row">
@@ -163,7 +166,7 @@
 
                         <!-- END 5 -->
 
-                        <div class="col-4">
+                        <div class="col-12 col-sm-6 col-xl-4">
                             <div class="rekomen-img">
                                 <img src="<?= base_url() ?>/assets/img/image.png" class="img-thumbnail" alt="...">
                                 <div class="row">
@@ -184,37 +187,41 @@
                         </div>
 
                         <!-- END 6 -->
-                        
                     </div>
                 </div>
 
-                <!-------------------------------------------- KATEGORI ------------------------------------------>
+                <!-- REKOMENDASI SECTION END -->
 
-                <div class="col-4">
+                <div class="col-12 col-lg-4">
                     <div class="row">
+
+                        <!-- KATEGORI SECTION -->
+
                         <div class="col-12">
                             <h5>Kategori</h5>
                         </div>
-                        <div class="col-4">
-                            <button type="button" class="btn btn-warning btn-kategori">Biologi</button>
+                        <div class="col-12 col-sm-6">
+                            <button type="button" class="btn btn-block btn-primary btn-kategori">Biologi</button>
                         </div>
-                        <div class="col-4">
-                            <button type="button" class="btn btn-warning btn-kategori">Biologi</button>
+                        <div class="col-12 col-sm-6">
+                            <button type="button" class="btn btn-block btn-primary btn-kategori">Biologi</button>
                         </div>
-                        <div class="col-4">
-                            <button type="button" class="btn btn-warning btn-kategori">Biologi</button>
+                        <div class="col-12 col-sm-6">
+                            <button type="button" class="btn btn-block btn-primary btn-kategori">Biologi</button>
                         </div>
-                        <div class="col-4">
-                            <button type="button" class="btn btn-warning btn-kategori">Biologi</button>
+                        <div class="col-12 col-sm-6">
+                            <button type="button" class="btn btn-block btn-primary btn-kategori">Biologi</button>
                         </div>
-                        <div class="col-4">
-                            <button type="button" class="btn btn-warning btn-kategori">Biologi</button>
+                        <div class="col-12 col-sm-6">
+                            <button type="button" class="btn btn-block btn-primary btn-kategori">Biologi</button>
                         </div>
-                        <div class="col-4">
-                            <button type="button" class="btn btn-warning btn-kategori">Biologi</button>
+                        <div class="col-12 col-sm-6">
+                            <button type="button" class="btn btn-block btn-primary btn-kategori">Bahasa Indonesia</button>
                         </div>
 
-                        <!----------------------------------------- TOP PLAYLIST ----------------------------------------->
+                        <!-- KATEGORI SECTION END -->
+
+                        <!-- TOP PLAYLIST SECTION -->
 
                         <div class="col-12">
                             <h5 style="margin: 40px 0 10px 0;">Top PlayList</h5>
@@ -222,78 +229,31 @@
 
                         <!-- START -->
 
-                        <div class="col-4">
+                        <div class="col-3 col-lg-4">
                             <div class="top-playlist">
-                                <img class="img-top-playlist" src="<?= base_url() ?>/assets/img/image.png" alt="">
+                                <img class="img-fluid" src="<?= base_url() ?>/assets/img/hero_img.png" alt="">
                             </div>
                         </div>
-                        <div class="col-5">
+                        <div class="col-9 col-lg-8 d-flex align-items-center">
                             <div class="row">
-                                <div class="col-12">
+                                <div class="col-10">
                                     <p class="judul-top-playlist"><b>Biologi - DNA</b></p>
-                                </div>
-                                <div class="col-12">
                                     <p class="views-top-playlist">200.000.000 Views</p>
                                 </div>
+                                <div class="col-2 d-flex">
+                                    <div class="col">
+                                        <i class="far fa-heart heart-top-playlist"></i>
+                                    </div>
+                                    <div class="col">
+                                        <i class="far fa-bookmark book-top-playlist"></i>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-1">
-                            <i class="far fa-heart heart-top-playlist"></i>
-                        </div>
-                        <div class="col-1">
-                            <i class="far fa-bookmark book-top-playlist"></i>
                         </div>
 
                         <!-- END 1 -->
 
-                        <div class="col-4">
-                            <div class="top-playlist">
-                                <img class="img-top-playlist" src="<?= base_url() ?>/assets/img/image.png" alt="">
-                            </div>
-                        </div>
-                        <div class="col-5">
-                            <div class="row">
-                                <div class="col-12">
-                                    <p class="judul-top-playlist"><b>Biologi - DNA</b></p>
-                                </div>
-                                <div class="col-12">
-                                    <p class="views-top-playlist">200.000.000 Views</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-1">
-                            <i class="far fa-heart heart-top-playlist"></i>
-                        </div>
-                        <div class="col-1">
-                            <i class="far fa-bookmark book-top-playlist"></i>
-                        </div>
-
-                        <!-- END 2 -->
-
-                        <div class="col-4">
-                            <div class="top-playlist">
-                                <img class="img-top-playlist" src="<?= base_url() ?>/assets/img/image.png" alt="">
-                            </div>
-                        </div>
-                        <div class="col-5">
-                            <div class="row">
-                                <div class="col-12">
-                                    <p class="judul-top-playlist"><b>Biologi - DNA</b></p>
-                                </div>
-                                <div class="col-12">
-                                    <p class="views-top-playlist">200.000.000 Views</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-1">
-                            <i class="far fa-heart heart-top-playlist"></i>
-                        </div>
-                        <div class="col-1">
-                            <i class="far fa-bookmark book-top-playlist"></i>
-                        </div>
-
-                        <!-- END 3 -->
-
+                        <!-- TOP PLAYLIST SECTION END -->
                     </div>
                 </div>
             </div>
